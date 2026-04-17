10cb03b6ad2678590aec3f56b90491ee1d66dbf7
Admin <Admin@DESKTOP-12J6D6C>
2026-04-17 09:47:02 +0700

    Add project files.

---
+++ PetLuv/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetLuv.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
